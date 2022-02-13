99e1f35e18fd34dd857ac1735ff4ab3eef42f09e
uboar <uboaruboar@gmail.com>
2022-02-13 17:27:20 +0900

    テスト運用中である旨を追記

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -2,7 +2,7 @@
   <v-row>
     <v-col />
     <v-col align-self="center" md="12">
-      <h1>UBO-MUSICS</h1>
+      <h1>UBO-MUSICS(テスト運用中)</h1>
       <VueGlow
         :color="$vuetify.theme.themes.dark.primary"
         elevation="10"
